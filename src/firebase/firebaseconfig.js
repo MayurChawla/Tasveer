0b2fa5bd5674f9b15d64e22ea91f05c6b923b9cf
MayurChawla <mr.mayurchawla@gmail.com>
2021-04-22 11:10:52 +0530

    removed firebase key

--- src/firebase/firebaseconfig.js
+++ src/firebase/firebaseconfig.js
@@ -2,13 +2,6 @@ import * as firebase from 'firebase/app';
 import 'firebase/storage';
 import 'firebase/firestore';
 // Your web app's Firebase configuration
- var firebaseConfig = {
-    apiKey: "AIzaSyCXGQjQASOnoXLKviNScab5-ulx4hrB20o",
-    authDomain: "tasveer-ec027.firebaseapp.com",
-    projectId: "tasveer-ec027",
-    storageBucket: "tasveer-ec027.appspot.com",
-    messagingSenderId: "957843283617",
-    appId: "1:957843283617:web:fc3cb0c649c62279031ae0"
-  };
+ 
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);

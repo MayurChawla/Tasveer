834c5ff35d8e718cce55c802f81ae15435d104bb
MayurChawla <mr.mayurchawla@gmail.com>
2021-04-22 11:09:11 +0530

    initialized firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <nav className="navigation">
+          <a className="nav-icon" href="/">@Tasveer</a>
+        </nav>
+        <hr className="nav-div-hr"/>
+        <header>
+          <h1 className="hero-heading">
+            Your Gallery
+          </h1>
+          <p className="hero-quote">
+            Add your home or family pics to your profile.
+          </p>
+        </header>
     </div>
   );
 }
